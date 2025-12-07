bf04a59e92cfa82f54f520ccf39760ed9b12593d
thesosu <przemek18032003@gmail.com>
2025-12-07 17:09:54 +0100

    init

---
+++ src/main/java/com/example/projektgit/ProjektGitApplication.java
@@ -0,0 +1,13 @@
+package com.example.projektgit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProjektGitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ProjektGitApplication.class, args);
+    }
+
+}
